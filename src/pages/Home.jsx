1240444bfbe8c5fa3effff2ea6364f19d9deaa69
Baptiste <baptistev59@free.fr>
2025-11-10 18:00:50 +0100

    modif de l'adresse de l'image en page d'accueil

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,7 @@
 export default function Home() {
     return (
         <div>
-            <h1>Home</h1>
-            <img src="./public/home.png" style={{ margin: "auto", width: "500px", maxWidth: "100%" }} />
+            <img src={`${import.meta.env.BASE_URL}home.png`} alt="Accueil" style={{ margin: "auto", width: "500px", maxWidth: "100%" }} />
         </div>
     )
 }
